175ca0e2a05f3877c5ca8e8a70b9d2390a8ca1f2
davis <davisochieng4@gmail.com>
2020-07-29 17:27:23 -0400

    Set up redux store ans the alert system

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/register';
 import Login from './components/auth/login';
+import store from './store';
+import Alert from './components/layout/alert';
 
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+import { setAlert } from '../../actions/alert';
+
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -16,7 +19,7 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('passwords do not match');
+      setAlert('passwords do not match', 'danger');
     } else {
       console.log(formData);
     }
@@ -80,4 +83,7 @@ const Register = () => {
     </Fragment>
   );
 };
-export default Register;
+Register.prototype = {
+  setAlert: PropTypes.func.isRequired,
+};
+export default connect(null, { setAlert })(Register);
